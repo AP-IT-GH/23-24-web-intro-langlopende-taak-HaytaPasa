585d20a93df175d5b501e38a757b986604dfea3c
HaytaPasa <160629191+HaytaPasa@users.noreply.github.com>
2024-05-10 13:33:27 +0200

    langlopendetaak
    v4

--- js/map.js
+++ js/map.js
@@ -1,9 +1,9 @@
-let map = L.map('map').setView([51.505, -0.09], 13);
+let map = L.map('itMap').setView([51.23009, 4.41616], 17);
 
-L.tileLayer('https://tile.openstreetmap.org/{zoom}/{x}/{y}.png', {
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-let marker = L.marker([51.505, -0.09]).addTo(map);
+let marker = L.marker([51.23009, 4.41616]).addTo(map);
   marker.bindPopup("Hello, I'm a marker!").openPopup();
